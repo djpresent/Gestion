9ead4fff617adeedae389002fc74036a16d6a721
admin <>
2016-12-05 15:29:42 +0000

    {/Prueba/src/main/java/ec/edu/ucuenca/prueba/Solicitud.java}

--- Prueba/src/main/java/ec/edu/ucuenca/prueba/Solicitud.java
+++ Prueba/src/main/java/ec/edu/ucuenca/prueba/Solicitud.java
@@ -5,28 +5,123 @@ package ec.edu.ucuenca.prueba;
  */
 
 @javax.persistence.Entity
-public class Solicitud implements java.io.Serializable {
+public class Solicitud implements java.io.Serializable
+{
 
-    static final long serialVersionUID = 1L;
+   static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "SOLICITUD_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "SOLICITUD_ID_SEQ", name = "SOLICITUD_ID_GENERATOR")
-    private java.lang.Long id;
+   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "SOLICITUD_ID_GENERATOR")
+   @javax.persistence.Id
+   @javax.persistence.SequenceGenerator(sequenceName = "SOLICITUD_ID_SEQ", name = "SOLICITUD_ID_GENERATOR")
+   @org.kie.api.definition.type.Label("Id")
+   private java.lang.Long id;
 
-    public Solicitud() {
-    }
-    
-    public Solicitud(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "Fecha de solicitud")
+   private java.util.Date fechaSolicitud;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "Fecha del Tr\u00E1mite")
+   private java.util.Date fechaTramite;
+
+   @javax.persistence.ElementCollection(fetch = javax.persistence.FetchType.EAGER)
+   @org.kie.api.definition.type.Label(value = "Esun documento de Quipux?")
+   private java.util.List<java.lang.Boolean> esQuipux;
+
+   @org.kie.api.definition.type.Label(value = "T\u00EDtulo de la solicutud")
+   private java.lang.String titulo;
+
+   @org.kie.api.definition.type.Label(value = "Finalizado?")
+   private java.lang.Boolean finalizado;
+
+   @org.kie.api.definition.type.Label(value = "Feca de finalizaci\u00F3n del tr\u00E1mite")
+   private java.util.Date fechaFin;
+
+   public Solicitud()
+   {
+   }
+
+   public java.lang.Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(java.lang.Long id)
+   {
+      this.id = id;
+   }
+
+   public java.util.Date getFechaSolicitud()
+   {
+      return this.fechaSolicitud;
+   }
+
+   public void setFechaSolicitud(java.util.Date fechaSolicitud)
+   {
+      this.fechaSolicitud = fechaSolicitud;
+   }
+
+   public java.util.Date getFechaTramite()
+   {
+      return this.fechaTramite;
+   }
+
+   public void setFechaTramite(java.util.Date fechaTramite)
+   {
+      this.fechaTramite = fechaTramite;
+   }
+
+   public java.util.List<java.lang.Boolean> getEsQuipux()
+   {
+      return this.esQuipux;
+   }
+
+   public void setEsQuipux(java.util.List<java.lang.Boolean> esQuipux)
+   {
+      this.esQuipux = esQuipux;
+   }
+
+   public java.lang.String getTitulo()
+   {
+      return this.titulo;
+   }
+
+   public void setTitulo(java.lang.String titulo)
+   {
+      this.titulo = titulo;
+   }
+
+   public java.lang.Boolean getFinalizado()
+   {
+      return this.finalizado;
+   }
+
+   public void setFinalizado(java.lang.Boolean finalizado)
+   {
+      this.finalizado = finalizado;
+   }
+
+   public java.util.Date getFechaFin()
+   {
+      return this.fechaFin;
+   }
+
+   public void setFechaFin(java.util.Date fechaFin)
+   {
+      this.fechaFin = fechaFin;
+   }
+
+   public Solicitud(java.lang.Long id, java.util.Date fechaSolicitud,
+         java.util.Date fechaTramite,
+         java.util.List<java.lang.Boolean> esQuipux,
+         java.lang.String titulo, java.lang.Boolean finalizado,
+         java.util.Date fechaFin)
+   {
+      this.id = id;
+      this.fechaSolicitud = fechaSolicitud;
+      this.fechaTramite = fechaTramite;
+      this.esQuipux = esQuipux;
+      this.titulo = titulo;
+      this.finalizado = finalizado;
+      this.fechaFin = fechaFin;
+   }
 
 }
