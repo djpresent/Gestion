c995d312afd77ad1cc0a4bf9cede1ac665437ef5
admin <>
2016-12-08 20:47:41 +0000

    Tramite {/DAF/src/main/java/ec/edu/ucuenca/daf/Tramite.java}

--- DAF/src/main/java/ec/edu/ucuenca/daf/Tramite.java
+++ DAF/src/main/java/ec/edu/ucuenca/daf/Tramite.java
@@ -5,28 +5,198 @@ package ec.edu.ucuenca.daf;
  */
 
 @javax.persistence.Entity
-public class Tramite implements java.io.Serializable {
+public class Tramite implements java.io.Serializable
+{
 
-    static final long serialVersionUID = 1L;
+   static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "TRAMITE_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "TRAMITE_ID_SEQ", name = "TRAMITE_ID_GENERATOR")
-    private java.lang.Long id;
+   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "TRAMITE_ID_GENERATOR")
+   @javax.persistence.Id
+   @javax.persistence.SequenceGenerator(sequenceName = "TRAMITE_ID_SEQ", name = "TRAMITE_ID_GENERATOR")
+   private java.lang.Long id;
 
-    public Tramite() {
-    }
-    
-    public Tramite(java.lang.Long id) {
-        this.id = id;
-    }
+   private java.util.Date fechaIngresoTramite;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+   private java.lang.String dependenciaOrigen;
+
+   private java.lang.String solicitante;
+
+   private java.lang.String fechaSolicitud;
+
+   private java.lang.String numeroDocumento;
+
+   private java.lang.String tipoDocumento;
+
+   private java.lang.Boolean esQuipux;
+
+   private java.lang.String descripcion;
+
+   private java.util.Date fechaDespachoDireccion;
+
+   private java.lang.Boolean finalizado;
+
+   private java.util.Date fechaFinalizacion;
+
+   @javax.persistence.OneToMany(fetch = javax.persistence.FetchType.EAGER, cascade = { javax.persistence.CascadeType.ALL })
+   private java.util.List<ec.edu.ucuenca.daf.Reasignacion> reasignacion;
+
+   public Tramite()
+   {
+   }
+
+   public java.lang.Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(java.lang.Long id)
+   {
+      this.id = id;
+   }
+
+   public java.util.Date getFechaIngresoTramite()
+   {
+      return this.fechaIngresoTramite;
+   }
+
+   public void setFechaIngresoTramite(java.util.Date fechaIngresoTramite)
+   {
+      this.fechaIngresoTramite = fechaIngresoTramite;
+   }
+
+   public java.lang.String getDependenciaOrigen()
+   {
+      return this.dependenciaOrigen;
+   }
+
+   public void setDependenciaOrigen(java.lang.String dependenciaOrigen)
+   {
+      this.dependenciaOrigen = dependenciaOrigen;
+   }
+
+   public java.lang.String getSolicitante()
+   {
+      return this.solicitante;
+   }
+
+   public void setSolicitante(java.lang.String solicitante)
+   {
+      this.solicitante = solicitante;
+   }
+
+   public java.lang.String getFechaSolicitud()
+   {
+      return this.fechaSolicitud;
+   }
+
+   public void setFechaSolicitud(java.lang.String fechaSolicitud)
+   {
+      this.fechaSolicitud = fechaSolicitud;
+   }
+
+   public java.lang.String getNumeroDocumento()
+   {
+      return this.numeroDocumento;
+   }
+
+   public void setNumeroDocumento(java.lang.String numeroDocumento)
+   {
+      this.numeroDocumento = numeroDocumento;
+   }
+
+   public java.lang.String getTipoDocumento()
+   {
+      return this.tipoDocumento;
+   }
+
+   public void setTipoDocumento(java.lang.String tipoDocumento)
+   {
+      this.tipoDocumento = tipoDocumento;
+   }
+
+   public java.lang.Boolean getEsQuipux()
+   {
+      return this.esQuipux;
+   }
+
+   public void setEsQuipux(java.lang.Boolean esQuipux)
+   {
+      this.esQuipux = esQuipux;
+   }
+
+   public java.lang.String getDescripcion()
+   {
+      return this.descripcion;
+   }
+
+   public void setDescripcion(java.lang.String descripcion)
+   {
+      this.descripcion = descripcion;
+   }
+
+   public java.util.Date getFechaDespachoDireccion()
+   {
+      return this.fechaDespachoDireccion;
+   }
+
+   public void setFechaDespachoDireccion(java.util.Date fechaDespachoDireccion)
+   {
+      this.fechaDespachoDireccion = fechaDespachoDireccion;
+   }
+
+   public java.lang.Boolean getFinalizado()
+   {
+      return this.finalizado;
+   }
+
+   public void setFinalizado(java.lang.Boolean finalizado)
+   {
+      this.finalizado = finalizado;
+   }
+
+   public java.util.Date getFechaFinalizacion()
+   {
+      return this.fechaFinalizacion;
+   }
+
+   public void setFechaFinalizacion(java.util.Date fechaFinalizacion)
+   {
+      this.fechaFinalizacion = fechaFinalizacion;
+   }
+
+   public java.util.List<ec.edu.ucuenca.daf.Reasignacion> getReasignacion()
+   {
+      return this.reasignacion;
+   }
+
+   public void setReasignacion(
+         java.util.List<ec.edu.ucuenca.daf.Reasignacion> reasignacion)
+   {
+      this.reasignacion = reasignacion;
+   }
+
+   public Tramite(java.lang.Long id, java.util.Date fechaIngresoTramite,
+         java.lang.String dependenciaOrigen, java.lang.String solicitante,
+         java.lang.String fechaSolicitud, java.lang.String numeroDocumento,
+         java.lang.String tipoDocumento, java.lang.Boolean esQuipux,
+         java.lang.String descripcion,
+         java.util.Date fechaDespachoDireccion,
+         java.lang.Boolean finalizado, java.util.Date fechaFinalizacion,
+         java.util.List<ec.edu.ucuenca.daf.Reasignacion> reasignacion)
+   {
+      this.id = id;
+      this.fechaIngresoTramite = fechaIngresoTramite;
+      this.dependenciaOrigen = dependenciaOrigen;
+      this.solicitante = solicitante;
+      this.fechaSolicitud = fechaSolicitud;
+      this.numeroDocumento = numeroDocumento;
+      this.tipoDocumento = tipoDocumento;
+      this.esQuipux = esQuipux;
+      this.descripcion = descripcion;
+      this.fechaDespachoDireccion = fechaDespachoDireccion;
+      this.finalizado = finalizado;
+      this.fechaFinalizacion = fechaFinalizacion;
+      this.reasignacion = reasignacion;
+   }
 
 }
