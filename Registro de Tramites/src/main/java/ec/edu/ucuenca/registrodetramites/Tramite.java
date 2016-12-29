8760f5ea6053209d5e27059959286414787f2bec
krisv <>
2016-12-29 14:29:11 +0000

    {/Registro de Tramites/src/main/java/ec/edu/ucuenca/registrodetramites/Tramite.java}

--- Registro de Tramites/src/main/java/ec/edu/ucuenca/registrodetramites/Tramite.java
+++ Registro de Tramites/src/main/java/ec/edu/ucuenca/registrodetramites/Tramite.java
@@ -5,28 +5,201 @@ package ec.edu.ucuenca.registrodetramites;
  */
 
 @javax.persistence.Entity
-public class Tramite implements java.io.Serializable {
+public class Tramite implements java.io.Serializable
+{
 
-    static final long serialVersionUID = 1L;
+   static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "TRAMITE_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "TRAMITE_ID_SEQ", name = "TRAMITE_ID_GENERATOR")
-    private java.lang.Long id;
+   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "TRAMITE_ID_GENERATOR")
+   @javax.persistence.Id
+   @javax.persistence.SequenceGenerator(sequenceName = "TRAMITE_ID_SEQ", name = "TRAMITE_ID_GENERATOR")
+   private java.lang.Long id;
 
-    public Tramite() {
-    }
-    
-    public Tramite(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "Dependencia")
+   private java.lang.String dependenciaOrigen;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "Descripcion")
+   private java.lang.String descripcion;
+
+   @org.kie.api.definition.type.Label(value = "Es Quipux")
+   private java.lang.Boolean esQuipux;
+
+   @org.kie.api.definition.type.Label(value = "Fecha de despacho")
+   private java.util.Date fechaDespacho;
+
+   @org.kie.api.definition.type.Label(value = "Fecha de finalizacion")
+   private java.util.Date fechaFinalizacion;
+
+   @org.kie.api.definition.type.Label(value = "Fecha de Ingreso")
+   private java.util.Date fechaIngresoTramite;
+
+   @org.kie.api.definition.type.Label(value = "Finalizado")
+   private java.lang.Boolean finalizado;
+
+   @org.kie.api.definition.type.Label(value = "Numero de documento")
+   private java.lang.String numeroDocumento;
+
+   @org.kie.api.definition.type.Label(value = "Solicitante")
+   private java.lang.String solicitante;
+
+   @javax.persistence.OneToMany(fetch = javax.persistence.FetchType.EAGER, cascade = { javax.persistence.CascadeType.ALL })
+   @org.kie.api.definition.type.Label(value = "Reasignacion")
+   private java.util.List<ec.edu.ucuenca.registrodetramites.Reasignacion> reasignacion;
+
+   @org.kie.api.definition.type.Label(value = "Fecha de solicitud")
+   private java.util.Date fechaSolicitud;
+
+   public Tramite()
+   {
+   }
+
+   public java.lang.Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(java.lang.Long id)
+   {
+      this.id = id;
+   }
+
+   public java.lang.String getDependenciaOrigen()
+   {
+      return this.dependenciaOrigen;
+   }
+
+   public void setDependenciaOrigen(java.lang.String dependenciaOrigen)
+   {
+      this.dependenciaOrigen = dependenciaOrigen;
+   }
+
+   public java.lang.String getDescripcion()
+   {
+      return this.descripcion;
+   }
+
+   public void setDescripcion(java.lang.String descripcion)
+   {
+      this.descripcion = descripcion;
+   }
+
+   public java.lang.Boolean getEsQuipux()
+   {
+      return this.esQuipux;
+   }
+
+   public void setEsQuipux(java.lang.Boolean esQuipux)
+   {
+      this.esQuipux = esQuipux;
+   }
+
+   public java.util.Date getFechaDespacho()
+   {
+      return this.fechaDespacho;
+   }
+
+   public void setFechaDespacho(java.util.Date fechaDespacho)
+   {
+      this.fechaDespacho = fechaDespacho;
+   }
+
+   public java.util.Date getFechaFinalizacion()
+   {
+      return this.fechaFinalizacion;
+   }
+
+   public void setFechaFinalizacion(java.util.Date fechaFinalizacion)
+   {
+      this.fechaFinalizacion = fechaFinalizacion;
+   }
+
+   public java.util.Date getFechaIngresoTramite()
+   {
+      return this.fechaIngresoTramite;
+   }
+
+   public void setFechaIngresoTramite(java.util.Date fechaIngresoTramite)
+   {
+      this.fechaIngresoTramite = fechaIngresoTramite;
+   }
+
+   public java.lang.Boolean getFinalizado()
+   {
+      return this.finalizado;
+   }
+
+   public void setFinalizado(java.lang.Boolean finalizado)
+   {
+      this.finalizado = finalizado;
+   }
+
+   public java.lang.String getNumeroDocumento()
+   {
+      return this.numeroDocumento;
+   }
+
+   public void setNumeroDocumento(java.lang.String numeroDocumento)
+   {
+      this.numeroDocumento = numeroDocumento;
+   }
+
+   public java.lang.String getSolicitante()
+   {
+      return this.solicitante;
+   }
+
+   public void setSolicitante(java.lang.String solicitante)
+   {
+      this.solicitante = solicitante;
+   }
+
+   public java.util.List<ec.edu.ucuenca.registrodetramites.Reasignacion> getReasignacion()
+   {
+      return this.reasignacion;
+   }
+
+   public void setReasignacion(
+         java.util.List<ec.edu.ucuenca.registrodetramites.Reasignacion> reasignacion)
+   {
+      this.reasignacion = reasignacion;
+   }
+
+   public java.util.Date getFechaSolicitud()
+   {
+      return this.fechaSolicitud;
+   }
+
+   public void setFechaSolicitud(java.util.Date fechaSolicitud)
+   {
+      this.fechaSolicitud = fechaSolicitud;
+   }
+
+   public Tramite(
+         java.lang.Long id,
+         java.lang.String dependenciaOrigen,
+         java.lang.String descripcion,
+         java.lang.Boolean esQuipux,
+         java.util.Date fechaDespacho,
+         java.util.Date fechaFinalizacion,
+         java.util.Date fechaIngresoTramite,
+         java.lang.Boolean finalizado,
+         java.lang.String numeroDocumento,
+         java.lang.String solicitante,
+         java.util.List<ec.edu.ucuenca.registrodetramites.Reasignacion> reasignacion,
+         java.util.Date fechaSolicitud)
+   {
+      this.id = id;
+      this.dependenciaOrigen = dependenciaOrigen;
+      this.descripcion = descripcion;
+      this.esQuipux = esQuipux;
+      this.fechaDespacho = fechaDespacho;
+      this.fechaFinalizacion = fechaFinalizacion;
+      this.fechaIngresoTramite = fechaIngresoTramite;
+      this.finalizado = finalizado;
+      this.numeroDocumento = numeroDocumento;
+      this.solicitante = solicitante;
+      this.reasignacion = reasignacion;
+      this.fechaSolicitud = fechaSolicitud;
+   }
 
 }
